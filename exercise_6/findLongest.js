3c510c09c252d59af16ad44a1f438dc96b9ec784
MrFejza <fejzaergis@gmail.com>
2025-03-21 14:33:06 +0100

    finished all tasks

--- exercise_6/findLongest.js
+++ exercise_6/findLongest.js
@@ -6,7 +6,7 @@ function findLongestWord(sentence) {
   for (let i = 0; i < words.length; i++) {
     if (words[i].length > longestWord.length) {
       longestWord = words[i];
-    }
+    } 
   }
   return longestWord;
 }
